3562b2e9183b4c8c29c83ae8044e2c56958e5bdb
Esunsin <jinchan1148@gmail.com>
2024-03-26 22:19:16 +0900

    :tada:

---
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/OeMosNaeMasMarketApplication.java
@@ -0,0 +1,13 @@
+package cheolppochwippo.oe_mos_nae_mas_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OeMosNaeMasMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OeMosNaeMasMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cheolppochwippo/oe_mos_nae_mas_market/OeMosNaeMasMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package cheolppochwippo.oe_mos_nae_mas_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OeMosNaeMasMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
