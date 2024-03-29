86dfcfb0bd4b883a7d85f26878944e2adbfaf72a
Esunsin <jinchan1148@gmail.com>
2024-03-29 22:26:18 +0900

    :tada: add : product

---
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package cheolppochwippo.oe_mos_nae_mas_market.domain.product.repository;
+
+import cheolppochwippo.oe_mos_nae_mas_market.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
