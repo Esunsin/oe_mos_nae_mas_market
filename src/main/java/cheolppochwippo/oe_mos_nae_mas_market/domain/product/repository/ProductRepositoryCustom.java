9bc6bd2256f4a967408a2c9874a6ea1494035b25
noeyodeel <sko8044@gmail.com>
2024-04-03 15:54:46 +0900

    :sparkles: feat : 상품 조회 Querydsl 추가

--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/product/repository/ProductRepositoryCustom.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/product/repository/ProductRepositoryCustom.java
@@ -1,9 +1,10 @@
 package cheolppochwippo.oe_mos_nae_mas_market.domain.product.repository;
 
 import cheolppochwippo.oe_mos_nae_mas_market.domain.product.entity.Product;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepositoryCustom {
-
+    List<Product> findProductsWithQuantityGreaterThanOne();
 }
--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/product/repository/ProductRepositoryCustomImpl.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/product/repository/ProductRepositoryCustomImpl.java
@@ -1,10 +1,26 @@
 package cheolppochwippo.oe_mos_nae_mas_market.domain.product.repository;
 
+import static cheolppochwippo.oe_mos_nae_mas_market.domain.product.entity.QProduct.product;
+import static cheolppochwippo.oe_mos_nae_mas_market.domain.store.entity.QStore.store;
+
+import cheolppochwippo.oe_mos_nae_mas_market.domain.product.entity.Product;
+import cheolppochwippo.oe_mos_nae_mas_market.global.config.JpaConfig;
+import cheolppochwippo.oe_mos_nae_mas_market.global.entity.enums.Deleted;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
 @Repository
 public class ProductRepositoryCustomImpl implements ProductRepositoryCustom{
-
+    private final JpaConfig jpaConfig;
+    @Override
+    public List<Product> findProductsWithQuantityGreaterThanOne() {
+        return jpaConfig.jpaQueryFactory()
+            .selectFrom(product)
+            .innerJoin(product.store, store)
+            .where(product.quantity.gt(1)
+                .and(product.deleted.eq(Deleted.UNDELETE)))
+            .fetch();
+    }
 }
