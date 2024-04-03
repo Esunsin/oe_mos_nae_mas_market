63d01dae812359e5253fcd61a5e6334de0604e0e
Esunsin <jinchan1148@gmail.com>
2024-04-03 20:46:49 +0900

    :fire: remove : 필요 없는 파일 삭제

--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/totalOrder/dto/TotalOrderRequest.java
+++
@@ -1,4 +0,0 @@
-package cheolppochwippo.oe_mos_nae_mas_market.domain.totalOrder.dto;
-
-public class TotalOrderRequest {
-}
--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/totalOrder/service/TotalOrderService.java
+++
@@ -1,4 +0,0 @@
-package cheolppochwippo.oe_mos_nae_mas_market.domain.totalOrder.service;
-
-public interface TotalOrderService {
-}
