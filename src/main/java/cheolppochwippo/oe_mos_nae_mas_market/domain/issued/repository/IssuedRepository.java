56e00b2396a52f5c68b739f3907ef0d03d4b894e
iminkyu <alsrb18080@gmail.com>
2024-04-03 20:45:48 +0900

    :zap: 쿠폰 발행 쿼리 최적화

--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/issued/repository/IssuedRepository.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/issued/repository/IssuedRepository.java
@@ -1,13 +1,8 @@
 package cheolppochwippo.oe_mos_nae_mas_market.domain.issued.repository;
 
 import cheolppochwippo.oe_mos_nae_mas_market.domain.issued.entity.Issued;
-import cheolppochwippo.oe_mos_nae_mas_market.domain.user.entity.User;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IssuedRepository extends JpaRepository<Issued,Long>{
+public interface IssuedRepository extends JpaRepository<Issued, Long>, IssuedRepositoryCustom {
 
-    List<Issued> findByCouponIdAndUser(Long couponId, User user);
-
-    List<Issued> findByUser(User user);
 }
--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/issued/service/IssuedServiceImpl.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/issued/service/IssuedServiceImpl.java
@@ -2,58 +2,72 @@ package cheolppochwippo.oe_mos_nae_mas_market.domain.issued.service;
 
 import cheolppochwippo.oe_mos_nae_mas_market.domain.coupon.entity.Coupon;
 import cheolppochwippo.oe_mos_nae_mas_market.domain.coupon.repository.CouponRepository;
-import cheolppochwippo.oe_mos_nae_mas_market.domain.coupon.service.CouponService;
 import cheolppochwippo.oe_mos_nae_mas_market.domain.issued.dto.IssuedResponse;
 import cheolppochwippo.oe_mos_nae_mas_market.domain.issued.entity.Issued;
 import cheolppochwippo.oe_mos_nae_mas_market.domain.issued.repository.IssuedRepository;
 import cheolppochwippo.oe_mos_nae_mas_market.domain.user.entity.User;
+import cheolppochwippo.oe_mos_nae_mas_market.global.config.RedisConfig;
+import cheolppochwippo.oe_mos_nae_mas_market.global.exception.ErrorCode;
+import cheolppochwippo.oe_mos_nae_mas_market.global.exception.customException.NotFoundException;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@EnableCaching
 public class IssuedServiceImpl implements IssuedService {
 
     private final IssuedRepository issuedRepository;
-    private final CouponService couponService;
     private final CouponRepository couponRepository;
+    private final RedisConfig redisConfig;
+    private final CacheManager cacheManager;
 
     @Override
     @Transactional
     public IssuedResponse issueCoupon(Long couponId, User user) {
-        Map<Long, Boolean> issuedCouponMap = issuedRepository.findByCouponIdAndUser(couponId, user)
-            .stream()
-            .collect(Collectors.toMap(issued -> issued.getCoupon().getId(), issued -> true));
+        RLock lock = redisConfig.redissonClient().getLock("couponLock" + couponId);
+        try {
+            lock.lock();
+            List<Issued> issuedCoupons = issuedRepository.findByCouponIdAndUser(couponId, user);
 
-        if (issuedCouponMap.containsKey(couponId)) {
-            throw new IllegalStateException("이미 쿠폰을 발급 받으셨습니다.");
-        }
-        Coupon coupon = couponService.findCoupon(couponId);
+            if (!issuedCoupons.isEmpty()) {
+                throw new IllegalStateException("이미 쿠폰을 발급 받으셨습니다.");
+            }
+            Coupon coupon = couponRepository.findById(couponId)
+                .orElseThrow(() -> new NotFoundException(
+                    ErrorCode.COUPON_NOT_FOUND));
 
-        if (coupon.getAmount() > 0) {
-            coupon.decreaseAmount();
-            couponRepository.save(coupon);
+            if (coupon.getAmount() > 0) {
+                coupon.decreaseAmount();
+                couponRepository.save(coupon);
 
-            Issued issuedCoupon = new Issued(coupon, user);
-            issuedRepository.save(issuedCoupon);
+                Issued issuedCoupon = new Issued(coupon, user);
+                issuedRepository.save(issuedCoupon);
 
-            return new IssuedResponse(couponId, coupon.getCouponInfo(),
-                issuedCoupon.getCreatedAt());
-        } else {
-            throw new IllegalArgumentException("남아있는 쿠폰이 없습니다.");
+                cacheManager.getCache("IssuedCoupon").put(couponId, issuedCoupon);
+
+                return new IssuedResponse(couponId, coupon.getCouponInfo(),
+                    issuedCoupon.getCreatedAt(), issuedCoupon.getDeleted());
+            } else {
+                throw new IllegalArgumentException("남아있는 쿠폰이 없습니다.");
+            }
+        } finally {
+            lock.unlock();
         }
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<IssuedResponse> getIssuedCoupons(User user) {
-        List<Issued> issuedCoupons = issuedRepository.findByUser(user);
-        return issuedCoupons.stream()
-            .map(IssuedResponse::new)
-            .collect(Collectors.toList());
+        return issuedRepository.findCouponByUser(user);
     }
 }
