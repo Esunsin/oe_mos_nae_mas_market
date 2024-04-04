ba2e154b9674c9fb8e3bb1106cf74f9b541e9583
RebbitK <andxl9979@gmail.com>
2024-04-04 14:16:39 +0900

    :sparkles: Exception CustomException 3종류 추가

---
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/global/exception/customException/NoEntityException.java
@@ -0,0 +1,7 @@
+package cheolppochwippo.oe_mos_nae_mas_market.global.exception.customException;
+
+public class NoEntityException extends RuntimeException {
+	public NoEntityException(String message) {
+		super(message);
+	}
+}
