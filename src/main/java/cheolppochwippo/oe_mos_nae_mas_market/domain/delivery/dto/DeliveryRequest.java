7dcafb0b4304840be5a0a8c8b0753fca85ca1d2a
iminkyu <alsrb18080@gmail.com>
2024-04-04 11:57:37 +0900

    :white_check_mark: Delivery 테스트 코드

--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/delivery/dto/DeliveryRequest.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/delivery/dto/DeliveryRequest.java
@@ -1,12 +1,14 @@
 package cheolppochwippo.oe_mos_nae_mas_market.domain.delivery.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 public class DeliveryRequest {
     private String address;
 }
