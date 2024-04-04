989d0c29244a9cad632ba88b727a5d142fbbf75a
RebbitK <andxl9979@gmail.com>
2024-04-04 14:24:08 +0900

    :recycle: exception GlobalException 처리

---
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/global/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package cheolppochwippo.oe_mos_nae_mas_market.global.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+	private String message;
+
+	private HttpStatus state;
+
+}
