6a8645630ed264e8b014f566bc895bcb2bc7bd7c
RebbitK <andxl9979@gmail.com>
2024-04-05 11:23:44 +0900

    :white_check_mark: payment 동시성제어 테스트 코드 현재 주석처리

---
+++ src/test/java/cheolppochwippo/oe_mos_nae_mas_market/payment/lock/PaymentLockTest.java
@@ -0,0 +1,53 @@
+package cheolppochwippo.oe_mos_nae_mas_market.payment.lock;
+
+import cheolppochwippo.oe_mos_nae_mas_market.domain.payment.dto.PaymentRequest;
+import cheolppochwippo.oe_mos_nae_mas_market.domain.payment.service.PaymentServiceImpl;
+import cheolppochwippo.oe_mos_nae_mas_market.domain.user.entity.User;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaymentLockTest {
+
+	@Autowired
+	PaymentServiceImpl paymentService;
+
+
+	@Test
+	@DisplayName("분산락테스트")
+	void redis() throws InterruptedException {
+//		int numThreads = 8;
+//		CountDownLatch latch = new CountDownLatch(numThreads);
+//		ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+//		User user = new User(1L);
+//		PaymentRequest paymentRequest = new PaymentRequest();
+//		paymentRequest.setAmount(2000L);
+//		paymentRequest.setOrderId("4ae6cde7-ed30-46be-asfas-2a01799f2c4fasdasdasd");
+//
+//		for (int i = 0; i < numThreads; i++) {
+//			int userId = i + 1; // 사용자 번호
+//			executor.submit(() -> {
+//				try {
+//					paymentService.checkPayment(user, paymentRequest);
+//					//productService.decreaseProductStock(order);
+//				} catch (Exception e) {
+//					System.out.println(e.getMessage());
+//				} finally {
+//					latch.countDown();
+//				}
+//			});
+//		}
+//
+//		// 모든 스레드가 실행을 완료할 때까지 대기
+//		latch.await(15, TimeUnit.SECONDS);
+
+	}
+
+
+}
