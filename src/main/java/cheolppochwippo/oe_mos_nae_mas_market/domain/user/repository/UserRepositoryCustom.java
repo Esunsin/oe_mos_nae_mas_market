dcbe64ba6a07796e289aa7fcac80154e8d4a5691
RebbitK <andxl9979@gmail.com>
2024-04-11 00:01:00 +0900

    :recycle: User 수신동의한 유저의 전화번호를 받는 쿼리문 추가

---
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/user/repository/UserRepositoryCustom.java
@@ -0,0 +1,10 @@
+package cheolppochwippo.oe_mos_nae_mas_market.domain.user.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepositoryCustom {
+
+	List<String> getPhoneNumberFindByConsentTrue();
+
+}
