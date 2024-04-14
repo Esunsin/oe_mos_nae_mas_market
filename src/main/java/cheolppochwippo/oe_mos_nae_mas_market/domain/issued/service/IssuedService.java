f3279068778d6d75255b60b337b20406f45fd7a8
iminkyu <alsrb18080@gmail.com>
2024-04-14 17:38:08 +0900

    :recycle: 쿠폰 발급 시 쿠폰 재고 감소되지 않게 변경, 락 삭제

--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/issued/service/IssuedService.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/issued/service/IssuedService.java
@@ -1,5 +1,6 @@
 package cheolppochwippo.oe_mos_nae_mas_market.domain.issued.service;
 
+import cheolppochwippo.oe_mos_nae_mas_market.domain.coupon.entity.Coupon;
 import cheolppochwippo.oe_mos_nae_mas_market.domain.issued.dto.IssuedResponse;
 import cheolppochwippo.oe_mos_nae_mas_market.domain.user.entity.User;
 import java.util.List;
@@ -9,4 +10,6 @@ public interface IssuedService {
     IssuedResponse issueCoupon(Long couponId, User user);
 
     List<IssuedResponse> getIssuedCoupons(User user);
+
+    void decreaseCouponAmount(Long couponId, User user);
 }
--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/issued/service/IssuedServiceImpl.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/issued/service/IssuedServiceImpl.java
@@ -11,15 +11,12 @@ import cheolppochwippo.oe_mos_nae_mas_market.global.exception.ErrorCode;
 import cheolppochwippo.oe_mos_nae_mas_market.global.exception.customException.NotFoundException;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBucket;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
@@ -33,41 +30,22 @@ public class IssuedServiceImpl implements IssuedService {
     private final IssuedRepository issuedRepository;
     private final CouponRepository couponRepository;
     private final RedisConfig redisConfig;
-    private final CacheManager cacheManager;
+    private final RedissonClient redissonClient;
 
     @Override
     public IssuedResponse issueCoupon(Long couponId, User user) {
-        RLock lock = redisConfig.redissonClient().getFairLock("couponLock" + couponId);
-        try {
-            boolean isLocked = lock.tryLock(10, 60, TimeUnit.SECONDS);
-            if (isLocked) {
-                try {
-                    if (isCouponAlreadyIssued(couponId, user.getId())) {
-                        throw new IllegalArgumentException("이미 쿠폰을 발급 받으셨습니다.");
-                    }
-                    Coupon coupon = getCouponById(couponId);
-                    if (coupon.getAmount() > 0) {
-                        decreaseCouponAmount(coupon);
-                        Issued issuedCoupon = saveIssuedCoupon(coupon, user);
-                        cacheIssuedCoupon(couponId, user.getId(), issuedCoupon, coupon.getEffective_date());
-                        return createIssuedResponse(couponId, coupon, issuedCoupon);
-                    } else {
-                        throw new IllegalArgumentException("남아있는 쿠폰이 없습니다.");
-                    }
-                } finally {
-                    lock.unlock();
-                }
-            } else {
-                throw new IllegalStateException("락을 획득하지 못했습니다.");
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException("락을 획득하는 동안 인터럽트가 발생했습니다.", e);
+        if (isCouponAlreadyIssued(couponId, user.getId())) {
+            throw new IllegalArgumentException("이미 쿠폰을 발급 받으셨습니다.");
         }
+        Coupon coupon = getCouponById(couponId);
+        Issued issuedCoupon = saveIssuedCoupon(coupon, user);
+        cacheIssuedCoupon(couponId, user.getId(), issuedCoupon, coupon.getEffective_date());
+        return createIssuedResponse(couponId, coupon, issuedCoupon);
     }
 
     private boolean isCouponAlreadyIssued(Long couponId, Long userId) {
-        RBucket<Issued> cacheBucket = redisConfig.redissonClient().getBucket("IssuedCoupon:" + couponId + ":" + userId);
+        RBucket<Issued> cacheBucket = redisConfig.redissonClient()
+            .getBucket("IssuedCoupon:" + couponId + ":" + userId);
         Issued issuedCoupon = cacheBucket.get();
         return issuedCoupon != null;
     }
@@ -76,28 +54,49 @@ public class IssuedServiceImpl implements IssuedService {
         return couponRepository.findById(couponId)
             .orElseThrow(() -> new NotFoundException(ErrorCode.COUPON_NOT_FOUND));
     }
-
-    private void decreaseCouponAmount(Coupon coupon) {
-        coupon.decreaseAmount();
-        couponRepository.save(coupon);
+    @Transactional
+    public void decreaseCouponAmount(Long issuedId, User user) {
+        RLock lock = redissonClient.getFairLock("issuedCoupon:" + issuedId + ":" + user);
+        try {
+            try {
+                boolean isLocked = lock.tryLock(10, 60, TimeUnit.SECONDS);
+                if (isLocked) {
+                    Issued issuedCoupon = issuedRepository.findById(issuedId)
+                        .orElseThrow(() -> new IllegalArgumentException("발급된 쿠폰이 없습니다."));
+                    Coupon coupon = issuedCoupon.getCoupon();
+                    if (coupon.getAmount() > 0) {
+                        coupon.decreaseAmount();
+                        couponRepository.save(coupon);
+                    } else {
+                        throw new IllegalArgumentException("남아있는 쿠폰이 없습니다.");
+                    }
+                }
+            } finally {
+                lock.unlock();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
-
     private Issued saveIssuedCoupon(Coupon coupon, User user) {
         Issued issuedCoupon = new Issued(coupon, user);
         return issuedRepository.save(issuedCoupon);
     }
 
-    private void cacheIssuedCoupon(Long couponId, Long userId, Issued issuedCoupon, LocalDateTime expirationDate) {
+    private void cacheIssuedCoupon(Long couponId, Long userId, Issued issuedCoupon,
+        LocalDateTime expirationDate) {
         LocalDateTime currentTime = LocalDateTime.now();
         Duration duration = Duration.between(currentTime, expirationDate);
         long timeToLive = duration.toMillis();
 
-        RBucket<Issued> cacheBucket = redisConfig.redissonClient().getBucket("IssuedCoupon:" + couponId + ":" + userId);
+        RBucket<Issued> cacheBucket = redisConfig.redissonClient()
+            .getBucket("IssuedCoupon:" + couponId + ":" + userId);
         cacheBucket.set(issuedCoupon, timeToLive, TimeUnit.MILLISECONDS);
     }
 
     private IssuedResponse createIssuedResponse(Long couponId, Coupon coupon, Issued issuedCoupon) {
-        return new IssuedResponse(issuedCoupon.getId(), couponId, coupon.getCouponInfo(), issuedCoupon.getCreatedAt(), issuedCoupon.getDeleted());
+        return new IssuedResponse(issuedCoupon.getId(), couponId, coupon.getCouponInfo(),
+            issuedCoupon.getCreatedAt(), issuedCoupon.getDeleted());
     }
 
 
@@ -107,4 +106,5 @@ public class IssuedServiceImpl implements IssuedService {
     public List<IssuedResponse> getIssuedCoupons(User user) {
         return issuedRepository.findCouponByUser(user);
     }
+
 }
