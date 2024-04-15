fe97624e685fba83b85aa7d17d12d698b68429b9
RebbitK <andxl9979@gmail.com>
2024-04-15 14:21:33 +0900

    :sparkles: exception 상황에 맞는 custom exception 추가

---
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/global/exception/customException/CouponAlreadyIssuedException.java
@@ -0,0 +1,7 @@
+package cheolppochwippo.oe_mos_nae_mas_market.global.exception.customException;
+
+public class CouponAlreadyIssuedException extends RuntimeException {
+	public CouponAlreadyIssuedException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/global/exception/customException/MinimumQuantityException.java
@@ -0,0 +1,8 @@
+package cheolppochwippo.oe_mos_nae_mas_market.global.exception.customException;
+
+public class MinimumQuantityException extends RuntimeException {
+	public MinimumQuantityException(String message) {
+		super(message);
+	}
+
+}
