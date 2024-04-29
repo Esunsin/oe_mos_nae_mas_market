1c08579171f01a08fd686beaa2a227ae56087ec6
noeyodeel <sko8044@gmail.com>
2024-04-29 19:17:42 +0900

    :sparkles: QuantityUpdateRequest productId 추가

--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/product/dto/QuantityUpdateRequest.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/product/dto/QuantityUpdateRequest.java
@@ -1,11 +1,14 @@
 package cheolppochwippo.oe_mos_nae_mas_market.domain.product.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class QuantityUpdateRequest {
+    private Long productId;
     private Long quantity;
 
 }
