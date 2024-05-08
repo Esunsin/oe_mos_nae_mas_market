7e7c6a2a01a99a6645e17c1d737b9ed36dfba74a
Esunsin <jinchan1148@gmail.com>
2024-05-08 16:12:30 +0900

    :fire: remove : 사용하지 않는 코드 삭제

--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/search/service/SearchService.java
+++
@@ -1,9 +0,0 @@
-package cheolppochwippo.oe_mos_nae_mas_market.domain.search.service;
-
-import cheolppochwippo.oe_mos_nae_mas_market.domain.search.document.ProductDocument;
-import java.util.List;
-
-public interface SearchService {
-
-    List<ProductDocument> searchProduct(String keyword);
-}
